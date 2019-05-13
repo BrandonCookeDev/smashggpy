4bc406b6937d6832940e821984f350e345274826
Brandon Cooke <brandoncookedev@gmail.com>
2019-05-12 22:43:09 -0400

    remove build dir, add build to gitignore. install twine

--- build/lib/src/common/Exceptions.py
+++
@@ -1,7 +0,0 @@
-class NotInitializedException(Exception):
-	def __init__(self, component_name):
-		self.message = '{} has not been initialized!'.format(component_name)
-
-class BadTokenException(Exception): 
-	def __init__(self, token='No token handed to Exception'):
-		self.message = 'Bad Token. Please input a 32 character hexidecimal string.\nGot input "{}"'.format(token)
