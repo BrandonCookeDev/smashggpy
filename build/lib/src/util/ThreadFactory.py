b86b7312038d419336125ddd64e8ff3ec047e997
Brandon Cooke <bcooke@resicap.com>
2019-05-12 22:43:09 -0400

    remove build dir, add build to gitignore. install twine

--- build/lib/src/util/ThreadFactory.py
+++
@@ -1,9 +0,0 @@
-from threading import Thread
-
-class ThreadFactory(object):
-
-	@staticmethod
-	def create(function, argument_dict):
-		return Thread(group=None, target=function, name=None, args=(), kwargs=argument_dict)
-
-from src.util.Logger import Logger
