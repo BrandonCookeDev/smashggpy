fba7ed9c47b32bd9022c2d65db67b9cfffb1a7c7
Brandon Cooke <brandoncookedev@gmail.com>
2019-06-16 10:05:28 -0400

    added mocked tournament object class

--- test/models/test_tournament.py
+++ test/models/test_tournament.py
@@ -2,6 +2,7 @@ import os
 import unittest
 import dotenv
 from pathlib import Path
+from unittest.mock import MagicMock, Mock, patch
 
 from smashggpy.util import Initializer
 from smashggpy.common import Common
@@ -11,8 +12,51 @@ from smashggpy.common.Exceptions import NoPhaseDataException
 from smashggpy.common.Exceptions import NoPhaseGroupDataException
 from smashggpy.models.Tournament import Tournament
 
+from smashggpy.util.NetworkInterface import NetworkInterface as NI
+
 BASE_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 ROOT_DIR = BASE_DIR/'..'/'..'
+
+GOOD_TOURNAMENT_DATA = {
+    "data": {
+        "tournament":{
+            "id": 6620,
+            "name": "Tipped Off 12 , Presented by The Lab Gaming Center!",
+            "slug": "tournament/tipped-off-12-presented-by-the-lab-gaming-center",
+            "city": "Atlanta",
+            "postalCode": "30339",
+            "addrState": "GA",
+            "countryCode": "US",
+            "region": "11",
+            "venueAddress": "2 Galleria Pkwy SE, Atlanta, GA 30339, USA",
+            "venueName": "The Cobb Galleria",
+            "gettingThere": None,
+            "lat": 33.8835141,
+            "lng": -84.4655017,
+            "timezone": "America/New_York",
+            "startAt": 1510401600,
+            "endAt": 1510549140,
+            "contactInfo": None,
+            "contactEmail": "thelabgaminginc@gmail.com",
+            "contactTwitter": "TheLabGamingCtr",
+            "contactPhone": "404-368-5274",
+            "ownerId": 11259
+        }
+    }
+}
+
+class MockedTournamentNI(object):
+
+    __mocked = None
+
+    @staticmethod
+    def setUp():
+        __mocked = NI()
+        __mocked.query = MagicMock(return_value=GOOD_TOURNAMENT_DATA)
+
+    @staticmethod
+    def tearDown():
+        __mocked = None
 
 class TestTournament(unittest.TestCase):
 
@@ -23,9 +67,11 @@ class TestTournament(unittest.TestCase):
     def setUp(self):
         dotenv.load_dotenv(dotenv_path=Path(ROOT_DIR, '.env'))
         Initializer.initialize(os.getenv('API_TOKEN'), 'info')
+        MockedTournamentNI.setUp()
 
     def tearDown(self):
         Initializer.uninitialize()
+        MockedTournamentNI.tearDown()
 
     def test_should_not_get_tournament_if_slug_is_empty(self):
         self.assertRaises(AssertionError, Tournament.get, None)
@@ -47,4 +93,9 @@ class TestTournament(unittest.TestCase):
 
     def test_should_not_get_phase_groups_if_tournament_has_no_id(self):
         self.assertRaises(AssertionError, self.bad_tournament.get_phase_groups)
+
+    def test_should_parse_tournament_data_correctly(self):
+        expected = Tournament()
+        actual = Tournament.parse(GOOD_TOURNAMENT_DATA)
+        self.assertEqual(expected, actual)
 
