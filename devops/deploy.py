21680e3cba2508c7de420f4308720661f5ecea70
Brandon Cooke <bcooke@resicap.com>
2019-06-27 09:25:32 -0400

    updated to version 0.0.26

--- devops/deploy.py
+++ devops/deploy.py
@@ -5,11 +5,13 @@ from os.path import abspath
 from pathlib import Path
 from subprocess import call
 
+ROOT_FOLDER = abspath(Path(__file__, '..', '..'))
+DEVOPS_FOLDER = abspath(Path(__file__, '..'))
 SETUP_PATH = abspath(Path(__file__, '..', '..', 'setup.py'))
 DEPLOY_SCRIPT_PATH = abspath(Path(__file__, '..', 'deploy.sh'))
 VERSION_REGEX = re.compile("(version=\"([0-9]+.[0-9]+.[0-9]+)\")")
 MAJOR_MINOR_PATCH_REGEX = re.compile("([0-9]+).([0-9]+).([0-9]+)")
-
+GIT_COMMIT_COMMAND = 'cd ' + ROOT_FOLDER + ' && git add . && git commit -m "{0}" && git push'
 
 def read_setup_py() -> str:
     lines = []
@@ -59,8 +61,32 @@ def write_version_to_setup_py(version: str):
         f.writelines(lines)
 
 
+def process_with_deployment():
+    response = input('Input [y] to continue with this deployment...')
+    if response != 'y':
+        print('Deployment canceled. Input was: {}'.format(response))
+        sys.exit(1)
+
+    write_version_to_setup_py(new_version)
+    call(DEPLOY_SCRIPT_PATH, shell=True)
+    print('deployment complete! Version: {}'.format(new_version))
+
+
+def do_git_commit():
+    do_commit_response = input('Would you like to commit to the git repository? [y] ')
+    if do_commit_response != 'y':
+        print('Not committing. Input was: {}'.format(do_commit_response))
+        sys.exit(0)
+
+    commit_message = input('Please type your commit message: ')
+    git_command = GIT_COMMIT_COMMAND.format(commit_message)
+    print(git_command)
+    call(git_command, shell=True)
+
+
 if __name__ == "__main__":
     print('beginning deployment')
+    print(GIT_COMMIT_COMMAND)
 
     major = sys.argv[1] if len(sys.argv) > 1 else 0
     minor = sys.argv[2] if len(sys.argv) > 2 else 0
@@ -72,11 +98,7 @@ if __name__ == "__main__":
     print('current version: {}'.format(version))
     print('new version after update: {}'.format(new_version))
 
-    response = input('Input [y] to continue with this deployment...')
-    if response != 'y':
-        print('Deployment canceled. Input was: {}'.format(response))
-        sys.exit(1)
+    # process_with_deployment()
+    do_git_commit()
 
-    write_version_to_setup_py(new_version)
-    call(DEPLOY_SCRIPT_PATH, shell=True)
-    print('deployment complete! Version: {}'.format(new_version))
+    print('Completed!')
